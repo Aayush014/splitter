1a2b5ff16869d2c89ba7e9448ed0e6053e273526
Aayush014 <aayushpatel1411@gmail.com>
2024-02-13 18:41:25 +0530

    13-2-24

--- lib/SplitterScreen.dart
+++ lib/SplitterScreen.dart
@@ -27,149 +27,165 @@ class _SplitterScreenState extends State<SplitterScreen> {
                 width: 500,
                 height: 610,
                 decoration: const BoxDecoration(color: Colors.deepOrange),
-                child: Column(children:
-                [
-                  const SizedBox(
-                    height: 13,
-                  ),
-                  Container(
-                    width: 370,
-                    height: 75,
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(8)),
-                    child: const Center(child: Text("1")),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    width: 370,
-                    height: 75,
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(8)),
-                    child: const Center(child: Text("2")),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    width: 370,
-                    height: 75,
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(8)),
-                    child: const Center(child: Text("3")),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    width: 370,
-                    height: 75,
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(8)),
-                    child: const Center(child: Text("4")),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    width: 370,
-                    height: 75,
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(8)),
-                    child: const Center(child: Text("5")),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    width: 370,
-                    height: 75,
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(8)),
-                    child: const Center(child: Text("6")),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    width: 370,
-                    height: 75,
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(8)),
-                    child: const Center(child: Text("7")),
-                  ),
-                ],
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 13,
+                    ),
+                    Container(
+                      width: 370,
+                      height: 75,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: const Center(child: Text("1")),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 370,
+                      height: 75,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: const Center(child: Text("2")),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 370,
+                      height: 75,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: const Center(child: Text("3")),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 370,
+                      height: 75,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: const Center(child: Text("4")),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 370,
+                      height: 75,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: const Center(child: Text("5")),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 370,
+                      height: 75,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: const Center(child: Text("6")),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 370,
+                      height: 75,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: const Center(child: Text("7")),
+                    ),
+                  ],
                 ),
               ),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
-                child: Container(height: 400,
+                child: Container(
+                  height: 380,
                   width: 520,
                   decoration: const BoxDecoration(color: Colors.deepOrange),
-                  child: Row(children: [
-                    const SizedBox(width: 10,),
-                    Container(
-                    height: 370,
-                    width: 75,
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(8)),
-                    child: const Center(child: Text("8")),
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 370,
+                        width: 75,
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: const Center(child: Text("8")),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 370,
+                        width: 75,
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: const Center(child: Text("9")),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 370,
+                        width: 75,
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: const Center(child: Text("10")),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 370,
+                        width: 75,
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: const Center(child: Text("11")),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 370,
+                        width: 75,
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: const Center(child: Text("12")),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 370,
+                        width: 75,
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: const Center(child: Text("13")),
+                      ),
+                    ],
                   ),
-                    const SizedBox(width: 10,),
-                    Container(
-                      height: 370,
-                      width: 75,
-                      decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(8)),
-                      child: const Center(child: Text("9")),
-                    ),
-                    const SizedBox(width: 10,),
-                    Container(
-                      height: 370,
-                      width: 75,
-                      decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(8)),
-                      child: const Center(child: Text("10")),
-                    ),
-                    const SizedBox(width: 10,),
-                    Container(
-                      height: 370,
-                      width: 75,
-                      decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(8)),
-                      child: const Center(child: Text("11")),
-                    ),
-                    const SizedBox(width: 10,),
-                    Container(
-                      height: 370,
-                      width: 75,
-                      decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(8)),
-                      child: const Center(child: Text("12")),
-                    ),
-                    const SizedBox(width: 10,),
-                    Container(
-                      height: 370,
-                      width: 75,
-                      decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(8)),
-                      child: const Center(child: Text("13")),
-                    ),
-                  ]),),
+                ),
               )
             ],
           ),
